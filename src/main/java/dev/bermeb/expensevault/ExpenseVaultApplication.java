01db18e18af34992f0a48a85ce095c565bd17bbd
Bernhard <bernhard@bermeb.dev>
2025-08-19 20:23:24 +0200

    init project

---
+++ src/main/java/dev/bermeb/expensevault/ExpenseVaultApplication.java
@@ -0,0 +1,13 @@
+package dev.bermeb.expensevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/bermeb/expensevault/ExpenseVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bermeb.expensevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
